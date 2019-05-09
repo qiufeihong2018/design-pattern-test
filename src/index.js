90c9b1608a84be2732a943ff0e4fa51636489b23
qiufeihong <qiufeihong2018@126.com>
2019-05-09 14:33:49 +0800

    设计原则

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-function loadImg(src) {
-  return new Promise(function (resolve, reject) {
-    let img = document.createElement('img')
-    img.onload = function () {
-      resolve(img)
-    }
-    img.onerror = function () {
-      reject('error')
-    }
-    img.src = src
-  })
+class Park {
+  constructor(ffreeCar, sfreeCar, tfreeCar, id, start, interval) {
+    this.ffreeCar = ffreeCar
+    this.sfreeCar = sfreeCar
+    this.tfreeCar = tfreeCar
+    this.id = id
+    this.start = start
+    this.interval = interval
+  }
+  watchCar() {
+    alert(`第一层的空余车位是${this.ffreeCar}\n第二层的空余车位是${this.sfreeCar}\n第三层的空余车位是${this.tfreeCar}`)
+  }
+  distinguishCar() {
+    alert(`该车车牌号是${this.id},开始停车时间是${this.start}`)
+  }
+  watchIdTime() {
+    alert(`该车车牌号是${this.id},停车时长为${this.interval}`)
+  }
 }
-let src = 'https://avatars1.githubusercontent.com/u/12479470?s=460&v=4'
-let res = loadImg(src)
-res.then(function (img) {
-  alert(`img's height:${img.height}`)
-  return img
-}).then(function (img) {
-  alert(`img's width:${img.width}`)
-  return img
-}).then(function(img){
-  alert(img.src)
-}).catch(function (ex) {
-  alert(`errorrrrr${ex}`)
-})
+const park = new Park(12, 43, 54, '1231r3454gfdf', '2019.1.1', '10h')
+park.watchCar()
+park.distinguishCar()
+park.watchIdTime()
