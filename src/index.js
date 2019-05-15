fe35ac978711ffe75ca01a5699388c5d8cbf8919
qiufeihong <qiufeihong2018@126.com>
2019-05-15 13:40:16 +0800

    add adapter

--- src/index.js
+++ src/index.js
@@ -1,23 +1,18 @@
-class Park {
-  constructor(ffreeCar, sfreeCar, tfreeCar, id, start, interval) {
-    this.ffreeCar = ffreeCar
-    this.sfreeCar = sfreeCar
-    this.tfreeCar = tfreeCar
-    this.id = id
-    this.start = start
-    this.interval = interval
-  }
-  watchCar() {
-    alert(`第一层的空余车位是${this.ffreeCar}\n第二层的空余车位是${this.sfreeCar}\n第三层的空余车位是${this.tfreeCar}`)
-  }
-  distinguishCar() {
-    alert(`该车车牌号是${this.id},开始停车时间是${this.start}`)
-  }
-  watchIdTime() {
-    alert(`该车车牌号是${this.id},停车时长为${this.interval}`)
+class Adaptee{
+  specificRequest(){
+    return '美国插座'
   }
 }
-const park = new Park(12, 43, 54, '1231r3454gfdf', '2019.1.1', '10h')
-park.watchCar()
-park.distinguishCar()
-park.watchIdTime()
+class Target{
+  constructor(name){
+    this.name=name
+    this.adaptee=new Adaptee
+  }
+  request(){
+    alert(`${this.name}-适配器-${this.adaptee.specificRequest()}`)
+  }
+}
+
+let target=new Target('中国插头')
+
+target.request()//中国插头-适配器-美国插座
