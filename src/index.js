36ce886796ed422f9e3df1bb00d1bb120f3da808
qiufeihong <qiufeihong2018@126.com>
2019-05-15 14:21:02 +0800

    add decorator demo1

--- src/index.js
+++ src/index.js
@@ -1,22 +1,10 @@
-class Car {
-  drive() {
-   alert('车能开')
+@testDec(false)
+class Demo{
+
+}
+function testDec(isDec){
+  return function(target){
+    target.isDec=isDec
   }
 }
-class Decorator {
-  constructor(car){
-    this.car=car
-  }
-  drive() {
-    this.car.drive()
-    this.sing(car)
-  }
-  sing(car){
-    alert('加一个音箱，来唱歌')
-  }
-}
-let car=new Car()
-car.drive()
-alert('----')
-let user=new Decorator(car)
-user.drive()
+alert(Demo.isDec)
