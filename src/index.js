10a630fbca2f14f3bdaa07312536589ab78d3a2c
qiufeihong <qiufeihong2018@126.com>
2019-05-15 14:32:36 +0800

    add @readonly

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
-@testDec(false)
-class Demo{
-
+function readonly(target,name,descriptor){
+  descriptor.writable=false
+  return descriptor
 }
-function testDec(isDec){
-  return function(target){
-    target.isDec=isDec
+class People{
+  constructor(name,age){
+    this.name=name
+    this.age=age
+  }
+
+  @readonly
+  getName(){
+    alert(`${this.name}是我的名字,${this.age}是我的年龄`)
   }
 }
-alert(Demo.isDec)
+let people=new People('qfh',123)
+people.getName()//qfh是我的名字,123是我的年龄
+people.getName=function(){
+  alert('update')
+}
+//Uncaught TypeError: Cannot assign to read only property 'getName' of object '#<People>'
