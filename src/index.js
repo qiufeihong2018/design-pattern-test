329e55a58f71e362204faffe79ef1fd9113693dd
qiufeihong <qiufeihong2018@126.com>
2019-05-21 15:29:10 +0800

    finsh agent proxy star

--- src/index.js
+++ src/index.js
@@ -1,26 +1,63 @@
-class Star {
-  constructor(name) {
-    this.name = name
-    this.call = this.call(name)
-  }
-  play() {
-    console.log(`${this.name}演出真好看`)
-  }
-  call() {
-    console.log(`我可以邀请${this.name}来演出吗`)
-  }
+// 明星
+let Star = {
+  name: 'zhuliye',
+  age: '12',
+  height: '167',
+  weight: '90',
+  price: 1230000,
+  phone: 'star_12355553333'
 }
-class ProxyStar {
-  constructor(name) {
-    this.star = new Star(name)
+// 经纪人
+let agent = new Proxy(Star, {
+  get: function (target, key) {
+    if (key === 'phone') {
+      // 返回经纪人电话
+      return 'agent_12355557666'
+    }
+    if (key === 'price') {
+      // 经纪人报价
+      return 2000000
+    }
+    return target[key]
+  },
+  set: function (target, key, val) {
+    if (key === 'cusPrice') {
+      if (val < 1000000) {
+        throw new Error('too low')
+      } else {
+        target[key] = val
+        return true
+      }
+    }
   }
-  play() {
-    this.star.play()
-  }
-}
+})
 // 测试
-let proxyStar=new ProxyStar('zhuliye')
-proxyStar.play()
+// console.log(agent.name)
+// console.log(agent.age)
+// console.log(agent.weight)
+// console.log(agent.price)
+// console.log(agent.phone)
+// agent.cusPrice=400000
+// console.log(agent.cusPrice)
+// zhuliye
+//   12
+//   90
+//   2000000
+//   agent_12355557666
+// Uncaught Error: too low
 
-// 我可以邀请zhuliye来演出吗
-// zhuliye演出真好看
+
+
+// console.log(agent.name)
+// console.log(agent.age)
+// console.log(agent.weight)
+// console.log(agent.price)
+// console.log(agent.phone)
+// agent.cusPrice=4000000
+// console.log(agent.cusPrice)
+// zhuliye
+// 12
+//  90
+//  2000000
+//  agent_12355557666
+//  4000000
