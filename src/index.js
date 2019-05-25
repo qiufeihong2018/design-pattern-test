9d09fa6df329a02a620bce11959e2148c919b353
qiufeihong2018 <15058301288@163.com>
2019-05-25 18:30:04 +0800

    add EventEmitter

--- src/index.js
+++ src/index.js
@@ -1,45 +1,14 @@
-// 主题，保存状态，状态变化之后触发所有的观察者对象
-class Project {
-  constructor() {
-    this.state = 0
-    this.observers = []
-  }
-  getState() {
-    return this.state
-  }
-  setState(state) {
-    this.state = state
-    this.notifyAllObservers()
-  }
-  notifyAllObservers() {
-    this.observers.forEach(observer => {
-      observer.update()
-    })
-  }
-  attach(observer) {
-    this.observers.push(observer)
-  }
-}
-// 观察者
-class Observer {
-  constructor(name, project) {
-    this.name = name
-    this.project = project
-    this.project.attach(this)
-  }
-  update(){
-    console.log(`${this.name},update,state:${this.project.getState()}`)
-  }
-}
-// 测试
-let p=new Project()
+const EventEmitter=require('events').EventEmitter
 
-let qfh=new Observer('qfh',p)
-let qfh1=new Observer('qfh1',p)
-let qfh2=new Observer('qfh2',p)
+const event=new EventEmitter()
+// 同一个事件上发生的两件事
+event.on('click',res=>{
+  console.log(`qfh${res}`)
+}).on('click',res=>{
+  console.log(`qfh2${res}`)
+})
 
-// 更新state的值
-p.setState(1)
-p.setState(2)
-p.setState(3)
-
+// 触发监听事件click
+event.emit('click','加油')
+// qfh加油
+// qfh2加油
