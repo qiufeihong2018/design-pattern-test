03c31182fe46bf642f136e9ab22c6e3498daa23c
qiufeihong <qiufeihong2018@126.com>
2019-05-29 18:11:01 +0800

    feat(增加commitizen):

--- src/index.js
+++ src/index.js
@@ -1,38 +1,41 @@
-// 颜色状态
-class State {
-    constructor(color) {
-        this.color = color
+import StateMachine from 'javascript-state-machine'
+import $ from 'jquery'
+var fsm=new StateMachine({
+    init:'收藏',
+    transitions:[{
+        name:'handleStore',
+        from:'收藏',
+        to:'取消收藏'
+    },{
+        name:'handleDelStore',
+        from:'取消收藏',
+        to:'收藏'
+    }],
+    methods: {
+        onHandleStore:()=>{
+            alert('收藏成功')
+            updateTxt()
+        },
+        onHandleDelStore:()=>{
+            alert('取消收藏成功')
+            updateTxt()        
+        }
     }
-    handle(content){
-        console.log(`现在是${this.color}灯`)
-        // 设置状态
-        content.setState(this)
-    }
+})
+
+let btn=$('#btn')
+// 按钮点击
+btn.click(function(){
+    fsm.is('收藏')?fsm.handleStore():fsm.handleDelStore()
+})
+
+// 按钮文字更新
+
+function updateTxt(){
+    btn.text(fsm.state)
 }
 
-// 主体
-class Content {
-    constructor() {
-        this.state = null
-    }
-    getState() {
-        return this.state
-    }
-    setState(state) {
-        this.state = state
-    }
-}
-// 测试
-let content = new Content()
-const red = new State('red')
-const yellow = new State('yellow')
-const green = new State('green')
+// 初始化
+updateTxt()
 
-red.handle(content)
-console.log(content.getState())
-
-yellow.handle(content)
-console.log(content.getState())
-
-green.handle(content)
-console.log(content.getState())
+// methods中的onHandleStore与transitions中的handleStore要对应且首字母大写，表示监听该方法
