a2d604feeb0c51be523bbc3cad11dc59baa6bacd
qiufeihong <qiufeihong2018@126.com>
2019-05-29 16:38:37 +0800

    feat(add status model): add status model
    add status model

--- src/index.js
+++ src/index.js
@@ -1,1 +1,38 @@
-console.log('totalLine')
+// 颜色状态
+class State {
+    constructor(color) {
+        this.color = color
+    }
+    handle(content){
+        console.log(`现在是${this.color}灯`)
+        // 设置状态
+        content.setState(this)
+    }
+}
+
+// 主体
+class Content {
+    constructor() {
+        this.state = null
+    }
+    getState() {
+        return this.state
+    }
+    setState(state) {
+        this.state = state
+    }
+}
+// 测试
+let content = new Content()
+const red = new State('red')
+const yellow = new State('yellow')
+const green = new State('green')
+
+red.handle(content)
+console.log(content.getState())
+
+yellow.handle(content)
+console.log(content.getState())
+
+green.handle(content)
+console.log(content.getState())
