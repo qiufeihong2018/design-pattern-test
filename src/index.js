d2dfd2bb83de85a7bf8aa5fbeb9780c4378b10a1
qiufeihong <qiufeihong2018@126.com>
2019-05-31 16:53:39 +0800

    feat(增加备忘录模式):

--- src/index.js
+++ src/index.js
@@ -1,34 +1,64 @@
-class Staff{
-    execute(){
-        console.log('入职')
+// 暂时备份
+class TmpBackup {
+    constructor(content) {
+        this.content = content
+    }
+    getContent() {
+        return this.content
     }
 }
 
-class HR{
-    constructor(staff){
-        this.staff=staff
+// 备份列表
+class Memento {
+    constructor() {
+        this.list = []
     }
-    email(){
-        console.log('offer给你了,来入职吧')
-        this.staff.execute()
+    add(tmpBackup) {
+        this.list.push(tmpBackup)
     }
-}
-
-class Boss{
-    constructor(hr){
-        this.hr=hr
-    }
-    decree(){
-        console.log('来入职吧')
-        this.hr.email()
+    get(index) {
+        return this.list[index]
     }
 }
 
-const staff=new Staff()
-const hr=new HR(staff)
-const boss=new Boss(hr)
-boss.decree()
+// 编辑器
+class Editor {
+    constructor() {
+        this.content = null
+    }
+    setContent(content) {
+        this.content = content
+    }
+    getContent() {
+        return this.content
+    }
+    // 保存
+    saveContentToTmpBackup() {
+        return new TmpBackup(this.content)
+    }
+    // 撤销
+    getContentFromTmpBackup(tmpBackup) {
+        this.content = tmpBackup.getContent()
+    }
+}
 
-// 来入职吧
-// offer给你了,来入职吧
-// 入职
+// 测试
+let editor=new Editor()
+let memento=new Memento()
+// 先填内容
+editor.setContent('aaaa')
+// 再将内容暂时保存
+let tmp=editor.saveContentToTmpBackup()
+// 最后将内容保存进备忘录
+memento.add(tmp)
+editor.setContent('bbbb')
+editor.setContent('cccc')
+editor.setContent('dddd')
+editor.setContent('eeee')
+editor.setContent('ffff')
+editor.setContent('gggg')
+
+console.log(editor.getContent())
+// 撤销到第一条
+editor.getContentFromTmpBackup(memento.get(0))
+console.log(editor.getContent())
