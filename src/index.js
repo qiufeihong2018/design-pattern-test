f4bc5a0640edcf0835eef06421458d11766ff9f2
qiufeihong <qiufeihong2018@126.com>
2019-06-05 17:39:52 +0800

    docs(职责链模式): git push

--- src/index.js
+++ src/index.js
@@ -1,53 +1,67 @@
-// 买家
-class Buyer {
-    constructor() {
-        this.number = 0
+function Handler() {
+    this.next = null;
+    this.setNext = function(_handler) {
+        this.next = _handler;
+    };
+
+    this.handleRequest = function(money) {
+
     }
-    setNum(num, m) {
-        this.number = num
-        if (m) {
-            m.setSellerNum()
+};
+
+//采购部
+function CGBHandler(){}
+CGBHandler.prototype = new Handler();
+CGBHandler.prototype.handleRequest = function(money) {
+     //处理权限最多10000
+    if (money < 10000){
+        console.log('同意');
+    } else {
+        console.log('金额太大，采购部只能处理一万以内的采购');
+        if (this.next) {
+            this.next.handleRequest(money);
         }
     }
-}
-// 卖家
-class Seller {
-    constructor() {
-        this.number = 0
-    }
-    setNum(num, m) {
-        this.number = num
-        if (m) {
-            m.setBuyerNum()
+};
+
+//总经理
+function ZJLHandler (){}
+ZJLHandler.prototype = new Handler();
+ZJLHandler.prototype.handleRequest = function(money) {
+     //处理权限最多100000
+    if (money < 100000){
+        console.log('10万以内的同意');
+    } else {
+        console.log('金额太大，总经理只能处理十万以内的采购');
+        if (this.next) {
+            this.next.handleRequest(money);
         }
     }
+};
+
+//董事长
+function DSZHandler(){}
+DSZHandler.prototype = new Handler();
+DSZHandler.prototype.handleRequest = function(money) {
+     //处理权限至少100000
+    if (money >= 100000){
+        console.log('10万以上的我董事长来处理');
+        //处理其他逻辑
+    } 
+};
+
+function Client() {
+    var cgb = new CGBHandler();
+    var zjl = new ZJLHandler();
+    var dsz = new DSZHandler();
+    cgb.setNext(zjl);
+    zjl.setNext(dsz);
+    
+    cgb.handleRequest(1004137);
 }
-// 中介者
-class Medium {
-    constructor(buyer, seller) {
-        this.buyer = buyer
-        this.seller = seller
-    }
-    setBuyerNum() {
-        let number = this.seller.number
-        this.buyer.setNum(number)
-    }
-    setSellerNum() {
-        let number = this.buyer.number
-        this.seller.setNum(number)
-    }
-}
 
-// 测试
-let buyer = new Buyer()
-let seller = new Seller()
-let medium = new Medium(buyer, seller)
-buyer.setNum(9000000,medium)
-console.log(`买家说出${seller.number}买这套房子`)
-//买家说出9000000买这套房子
+Client()
 
-seller.setNum(10000000,medium)
-console.log(`卖家说没有${buyer.number}不卖这套房子`)
-// 卖家说没有10000000不卖这套房子
-
-
+// 金额太大，采购部只能处理一万以内的采购
+// 金额太大，总经理只能处理十万以内的采购
+// 10万以上的我董事长来处理
