3038ef66403aa881f9522f7733d31bf49524ab9e
qiufeihong <qiufeihong2018@126.com>
2019-06-05 18:04:50 +0800

    feat(职责链模式实现高级规则匹配):

--- src/index.js
+++ src/index.js
@@ -4,64 +4,49 @@ function Handler() {
         this.next = _handler;
     };
 
-    this.handleRequest = function(money) {
+    this.handleRequest = function(event) {
 
     }
 };
 
 //采购部
-function CGBHandler(){}
-CGBHandler.prototype = new Handler();
-CGBHandler.prototype.handleRequest = function(money) {
-     //处理权限最多10000
-    if (money < 10000){
-        console.log('同意');
+function rule139Handler(){}
+rule139Handler.prototype = new Handler();
+rule139Handler.prototype.handleRequest = function(event) {
+     //处理权限最多1004139
+    if (event===1004139){
+        console.log('1004139匹配正确');
     } else {
-        console.log('金额太大，采购部只能处理一万以内的采购');
+        console.log('1004139匹配错误');
         if (this.next) {
-            this.next.handleRequest(money);
+            this.next.handleRequest(event);
         }
     }
 };
 
 //总经理
-function ZJLHandler (){}
-ZJLHandler.prototype = new Handler();
-ZJLHandler.prototype.handleRequest = function(money) {
-     //处理权限最多100000
-    if (money < 100000){
-        console.log('10万以内的同意');
+function rule136Handler (){}
+rule136Handler.prototype = new Handler();
+rule136Handler.prototype.handleRequest = function(event) {
+     //处理权限最多1004136
+    if (event === 1004136){
+        console.log('1004136匹配正确');      
     } else {
-        console.log('金额太大，总经理只能处理十万以内的采购');
+        console.log('1004136匹配错误');       
         if (this.next) {
-            this.next.handleRequest(money);
+            this.next.handleRequest(event);
         }
     }
 };
 
-//董事长
-function DSZHandler(){}
-DSZHandler.prototype = new Handler();
-DSZHandler.prototype.handleRequest = function(money) {
-     //处理权限至少100000
-    if (money >= 100000){
-        console.log('10万以上的我董事长来处理');
-        //处理其他逻辑
-    } 
-};
 
 function Client() {
-    var cgb = new CGBHandler();
-    var zjl = new ZJLHandler();
-    var dsz = new DSZHandler();
+    var cgb = new rule139Handler();
+    var zjl = new rule136Handler();
     cgb.setNext(zjl);
-    zjl.setNext(dsz);
     
     cgb.handleRequest(1004137);
 }
 
 Client()
 
-// 金额太大，采购部只能处理一万以内的采购
-// 金额太大，总经理只能处理十万以内的采购
-// 10万以上的我董事长来处理
